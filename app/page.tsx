b0fb1b74ae8683efbbd602338c7383190d978bb8
dsomorov <dsomorov@gmail.com>
2024-01-31 18:25:17 -0600

    add technology descriptions and links to main page

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
   }
 
   return (
-    <>
+    <div className="flex flex-col h-full max-h-screen">
       <header className="mb-4">
         <h1 className="text-[4rem] text-center">DoodleConnect</h1>
         <h2 className="text-[2rem] text-center">
@@ -35,9 +35,7 @@ export default function Home() {
         </h2>
       </header>
       <hr className="border-black" />
-      <div className="flex justify-center w-[1000px] mx-auto my-[40px] gap-[40px]">
-        {/* <ScrollArea className="h-[400px] pr-4"> */}
-
+      <div className="flex justify-center w-[1000px] mx-auto my-[40px] gap-[40px] grow">
         <section id="action-section" className="">
           <div className="sticky w-[400px] top-[50%] right-[50%] -translate-y-1/2">
             <div className="h-fit border-[1px] space-y-4 p-4 border-black flex flex-col">
@@ -62,72 +60,162 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section id="about-section" className="space-y-4">
-          <p>
-            Welcome to DoodleConnect! This project is a small tech demo for a
-            number of technologies I was recently exploring, each of which has a
-            link and description below.
-          </p>
-          <p>
-            DoodleConnect is a party chat app with live video and audio feed.
-            Each lobby also has a shared canvas where drawings from your friends
-            are displayed in realtime. Use the buttons to create a new lobby or
-            enter the link provided to you for an existing lobby.
-          </p>
-          <div>
-            <div className="flex space-x-2">
-              <p>Connect with me:</p>
-              <Link href="https://www.linkedin.com/in/daniel-somorov-05705313b/">
-                LinkedIn
-              </Link>
-              <Link href="https://github.com/Somorovd">GitHub</Link>
-              <Link href="https://somorovd.github.io/">Portfolio</Link>
+        <ScrollArea className="h-full pr-4">
+          <section id="about-section" className="space-y-4">
+            <p>
+              Welcome to DoodleConnect! This project is a small tech demo for a
+              number of technologies I was recently exploring, each of which has
+              a link and description below.
+            </p>
+            <p>
+              DoodleConnect is a party chat app with live video and audio feed.
+              Each lobby also has a shared canvas where drawings from your
+              friends are displayed in realtime. Use the buttons to create a new
+              lobby or enter the link provided to you for an existing lobby.
+            </p>
+            <div>
+              <div className="flex space-x-2">
+                <p>Connect with me:</p>
+                <Link href="https://www.linkedin.com/in/daniel-somorov-05705313b/">
+                  LinkedIn
+                </Link>
+                <Link href="https://github.com/Somorovd">GitHub</Link>
+                <Link href="https://somorovd.github.io/">Portfolio</Link>
+              </div>
+              <div className="flex space-x-2">
+                <p>Source Code:</p>
+                <Link href="https://github.com/Somorovd/DoodleConnect">
+                  GitHub
+                </Link>
+              </div>
             </div>
-            <div className="flex space-x-2">
-              <p>Source Code:</p>
-              <Link href="https://github.com/Somorovd/DoodleConnect">
-                GitHub
-              </Link>
+            <hr className="border-black" />
+            <div>
+              <Accordion type="single" collapsible>
+                <TechAccordionIcon tech="NextJS" link="https://nextjs.org/docs">
+                  <p>
+                    Next.js is a React framework for developing modern full
+                    stack web applications. Features include file-based routing,
+                    client- and server-side rendering, and data fetching with
+                    caching and revalidation.
+                  </p>
+                </TechAccordionIcon>
+                <TechAccordionIcon
+                  tech="TypeScript"
+                  link="https://www.typescriptlang.org/docs/handbook/typescript-from-scratch.html"
+                >
+                  <p>
+                    TypeScript is a statically typed superset of JavaScript that
+                    enhances the development experience by introducing strong
+                    typing, enabling developers to catch errors early in the
+                    development process.It provides improved code
+                    maintainability, better tooling support, and facilitates the
+                    creation of scalable and robust applications. TypeScript
+                    compiles to plain JavaScript, making it compatible with
+                    existing JavaScript codebases, while its additional features
+                    such as interfaces and advanced type checking contribute to
+                    more reliable and efficient code.
+                  </p>
+                </TechAccordionIcon>
+                <TechAccordionIcon
+                  tech="MongoDB"
+                  link="https://www.mongodb.com/"
+                >
+                  <p>
+                    MongoDB is a widely-used NoSQL database for efficiently
+                    storing and managing large volumes of diverse data types
+                    utilizing a flexible and scalable JSON-like
+                    document-oriented structure.
+                  </p>
+                </TechAccordionIcon>
+                <TechAccordionIcon
+                  tech="Mongoose"
+                  link="https://mongoosejs.com/docs/"
+                >
+                  <p>
+                    Mongoose is a MongoDB object modeling tool designed for
+                    Node.js applications. It provides a schema-based solution
+                    for interacting with MongoDB databases, making it easier to
+                    manage data relationships, perform validations, and
+                    streamline queries.
+                  </p>
+                </TechAccordionIcon>
+                <TechAccordionIcon
+                  tech="Tailwind CSS"
+                  link="https://tailwindcss.com/docs/installation"
+                >
+                  <p>
+                    Tailwind CSS is a utility-first CSS framework that
+                    streamlines the process of designing and styling websites.
+                    It provides a set of pre-built, low-level utility classes
+                    that can be easily applied directly in the HTML markup,
+                    allowing for rapid and efficient development. With a focus
+                    on simplicity and flexibility, Tailwind eliminates the need
+                    for writing custom CSS, making it a practical choice for
+                    developers aiming to build responsive and modern user
+                    interfaces.
+                  </p>
+                </TechAccordionIcon>
+                <TechAccordionIcon
+                  tech="Zustand"
+                  link="https://github.com/pmndrs/zustand"
+                >
+                  <p>
+                    Zustand is a lightweight state management library for React
+                    applications. It offers a simple API for managing global and
+                    local state. With its minimalistic design and efficient
+                    updates, Zustand provides developers with an easy-to-use
+                    solution for state management in their projects.
+                  </p>
+                </TechAccordionIcon>
+                <TechAccordionIcon
+                  tech="Clerk"
+                  link="https://clerk.com/docs/quickstarts/nextjs"
+                >
+                  <p>
+                    Clerk is a comprehensive user management platform with
+                    flexible API and embedabble UI to handle user authentication
+                    and security.
+                  </p>
+                </TechAccordionIcon>
+                <TechAccordionIcon
+                  tech="PartyKit"
+                  link="https://docs.partykit.io/quickstart/"
+                >
+                  <p>
+                    Partykit is a websockets library that facilitates real-time
+                    communication between a web server and clients and
+                    simplifies the development of multiplayer applications.
+                  </p>
+                </TechAccordionIcon>
+                <TechAccordionIcon
+                  tech="LiveKit"
+                  link="https://docs.livekit.io/realtime/quickstarts/nextjs-13/"
+                >
+                  <p>
+                    LiveKit is the open source stack for streaming audio, video,
+                    and data. LiveKit works across many platforms, and has
+                    prebuilt components and hooks for use with React and NextJS.
+                  </p>
+                </TechAccordionIcon>
+                <TechAccordionIcon
+                  tech="Shadcn/ui"
+                  link="https://ui.shadcn.com/"
+                >
+                  <p>
+                    Shadcn/UI is a lightweight and user-friendly UI framework
+                    that simplifies the development of interactive and
+                    responsive web applications. It offers a range of
+                    pre-designed components and styling options, streamlining
+                    the creation of modern and visually appealing user
+                    interfaces.
+                  </p>
+                </TechAccordionIcon>
+              </Accordion>
             </div>
-          </div>
-          <hr className="border-black" />
-          <div>
-            <Accordion type="single" collapsible>
-              <TechAccordionIcon tech="NextJS">
-                <p>NextJS</p>
-              </TechAccordionIcon>
-              <TechAccordionIcon tech="TypeScript">
-                <p>TypeScript</p>
-              </TechAccordionIcon>
-              <TechAccordionIcon tech="MongoDB">
-                <p>MongoDB</p>
-              </TechAccordionIcon>
-              <TechAccordionIcon tech="Mongoose">
-                <p>Mongoose</p>
-              </TechAccordionIcon>
-              <TechAccordionIcon tech="Tailwind">
-                <p>Tailwind</p>
-              </TechAccordionIcon>
-              <TechAccordionIcon tech="Zustand">
-                <p>Zustand</p>
-              </TechAccordionIcon>
-              <TechAccordionIcon tech="Clerk">
-                <p>Clerk</p>
-              </TechAccordionIcon>
-              <TechAccordionIcon tech="PartyKit">
-                <p>PartyKit</p>
-              </TechAccordionIcon>
-              <TechAccordionIcon tech="LiveKit">
-                <p>LiveKit</p>
-              </TechAccordionIcon>
-              <TechAccordionIcon tech="Shadcn/ui">
-                <p>Shadcn/ui</p>
-              </TechAccordionIcon>
-            </Accordion>
-          </div>
-        </section>
-        {/* </ScrollArea> */}
+          </section>
+        </ScrollArea>
       </div>
-    </>
+    </div>
   );
 }
--- components/tech-accordion-item.tsx
+++ components/tech-accordion-item.tsx
@@ -3,18 +3,29 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import Link from "next/link";
 import React, { ReactNode } from "react";
 
 type TechAccordionIconProps = {
   tech: string;
+  link: string;
   children: ReactNode;
 };
 
-const TechAccordionIcon = ({ tech, children }: TechAccordionIconProps) => {
+const TechAccordionIcon = ({
+  tech,
+  link,
+  children,
+}: TechAccordionIconProps) => {
   return (
     <AccordionItem value={tech}>
       <AccordionTrigger>{tech}</AccordionTrigger>
-      <AccordionContent>{children}</AccordionContent>
+      <AccordionContent>
+        {children}
+        <Link href={link} className="text-blue-500 mt-2 hover:underline">
+          More Information
+        </Link>
+      </AccordionContent>
     </AccordionItem>
   );
 };
