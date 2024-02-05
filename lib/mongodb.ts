408fdb4f03efbbdd08cf71d07a64fa809b4bd5cf
dsomorov <dsomorov@gmail.com>
2024-02-05 15:00:00 -0600

    dbg 08

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI!);
+    await mongoose.connect(process.env.MONGODB_URI!, {
+      retryWrites: true,
+    });
   } catch (error) {
     console.log("Error connecting to MongoDB\n", error);
   }
